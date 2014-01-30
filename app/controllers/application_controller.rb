2de8f917c620ca03afe9f7e807c26f028dc198e0
David Aragon <david@quickleft.com>
2014-01-29 19:55:02 -0700

    blah

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_user
+    @current_user ||= Brewer.find(session[:brewer_id]) if session[:brewer_id]
+  end
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Beertradr::Application.routes.draw do
-  devise_for :brewers
-
   resources :brews
 
 
